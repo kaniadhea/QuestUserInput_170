9b0f205c08c0d6b175fe4798e9f0a86649258f59
kaniadhea <kaniadhea7@gmail.com>
2024-10-30 11:51:35 +0700

    commit pertama

---
+++ app/src/main/java/com/example/tugas4/Latiihaninput.kt
@@ -0,0 +1,170 @@
+package com.example.tugas4
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun Latihaninput(modifier: Modifier = Modifier) {
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var noHp by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+
+    var dataNama by remember { mutableStateOf("") }
+    var dataEmail by remember { mutableStateOf("") }
+    var dataNoHp by remember { mutableStateOf("") }
+    var dataAlamat by remember { mutableStateOf("") }
+
+    var jenisK by remember { mutableStateOf("") }
+    var dataJenisK by remember { mutableStateOf("") }
+    val dataJK = listOf("Laki - laki", "Perempuan")
+
+    Column (modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = "Biodata",
+            fontWeight = FontWeight.Bold,
+            fontSize = 28.sp)
+        Spacer(modifier = Modifier.padding(20.dp))
+        TextField(
+            value = nama,
+            onValueChange = {nama = it},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            label = {
+                Text("Nama")
+            },
+            placeholder = {
+                Text("Masukkan Nama Anda")
+            }
+
+        )
+        Row {
+            dataJK.forEach { selectedJK ->
+                Row {
+                    RadioButton(selected = jenisK == selectedJK,
+                        onClick = {
+                            jenisK = selectedJK
+                        })
+                    Text(selectedJK)
+
+                }
+
+            }
+        }
+
+        TextField(
+            value = email,
+            onValueChange = {email = it},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            label = {
+                Text("Email")
+            },
+            placeholder = {
+                Text("Masukkan Email Anda")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+
+        )
+        TextField(
+            value = noHp,
+            onValueChange = {noHp = it},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            label = {
+                Text("Nomor Handphone")
+            },
+            placeholder = {
+                Text("Masukkan Nomor Handphone Anda")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+        )
+        TextField(
+            value = alamat,
+            onValueChange = {alamat = it},
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            label = {
+                Text("alamat")
+            },
+            placeholder = {
+                Text("Masukkan alamat Anda")
+            }
+
+        )
+        Button(onClick = {dataNama = nama
+            dataEmail = email
+            dataNoHp = noHp
+            dataAlamat = alamat
+                         },modifier = Modifier.padding(vertical = 10.dp)) {
+            Text("Simpan")
+        }
+        Card(modifier=Modifier.fillMaxWidth().padding(16.dp)) {
+            Column {
+                TampilData(
+                    judul = "Nama",
+                    isinya = dataNama
+                )
+                TampilData(
+                    judul = "jenisK",
+                    isinya = dataJenisK
+                )
+                TampilData(
+                    judul = "email",
+                    isinya = dataEmail
+                )
+                TampilData(
+                    judul = "noHp",
+                    isinya = dataNoHp
+                )
+                TampilData(
+                    judul = "alamat",
+                    isinya = dataAlamat
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun TampilData(
+    judul : String,
+    isinya : String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = judul, modifier = Modifier.weight(0.8f))
+        Text(text = ":", modifier = Modifier.weight(0.2f))
+        Text(
+            text = isinya,
+            modifier = Modifier.weight(2f)
+        )
+    }
+}
